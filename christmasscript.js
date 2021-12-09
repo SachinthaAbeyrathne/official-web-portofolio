346f634e7eb81eccdc27d49f61cc5e10f435abfc
Sachintha Abeyrathne <32412711+SachinthaAbeyrathne@users.noreply.github.com>
2021-12-09 16:12:21 +0530

    Update christmasscript.js

--- christmasscript.js
+++ christmasscript.js
@@ -1,34 +1,62 @@
-      function countdown() {
-        var now = new Date();
-        var eventDate = new Date(2020, 11, 25);
+const days = document.getElementById('days'),
+      minutes = document.getElementById('minutes'),
+      hours = document.getElementById('hours'),
+      seconds = document.getElementById('seconds');
+let xYear = 2021;
+setInterval(() => {
+    findDate();
+}, 1000);
 
-        var currentTime = now.getTime();
-        var eventTime = eventDate.getTime();
+function findDate(){
+    let currentTime = new Date(),
+        christmasYear = currentTime.getFullYear();
 
-        var remTime = eventTime - currentTime;
+    // getMonth() method returns the month (from 0 to 11)
+    if(currentTime.getMonth() == 0 && currentTime.getDate() > 1){
+        christmasYear += 1; 
+    }
 
-        var s = Math.floor(remTime / 1000);
-        var m = Math.floor(s / 60);
-        var h = Math.floor(m / 60);
-        var d = Math.floor(h / 24);
+    let christmasTime = new Date(christmasYear, 0, 01); 
+    let dateDiff = Math.floor(christmasTime - currentTime);
 
-        h %= 24;
-        m %= 60;
-        s %= 60;
+    let DAYS = 0, HOURS = 0, MINUTES = 0, SECONDS = 0;
 
-        h = h < 10 ? "0" + h : h;
-        m = m < 10 ? "0" + m : m;
-        s = s < 10 ? "0" + s : s;
+    if(currentTime.getMonth() != 0 || (currentTime.getMonth() == 0 && currentTime.getDate() != 1)){
+        DAYS = Math.floor(dateDiff / (1000 * 60 * 60 * 24));
+        HOURS = Math.floor((dateDiff) % (1000 * 60 * 60 * 24) / (1000 * 60 * 60));
+        MINUTES = Math.floor((dateDiff % (1000 * 60 * 60)) / (1000 * 60));
+        SECONDS = Math.floor((dateDiff) % (1000 * 60) / 1000);
+    }
+    displayDate(setZero(DAYS), setZero(HOURS), setZero(MINUTES), setZero(SECONDS));
+}
 
-        document.getElementById("days").textContent = d;
-        document.getElementById("days").innerText = d;
+function displayDate(d, h, m, s){
+    days.innerHTML = d;
+    hours.innerHTML = h;
+    minutes.innerHTML = m;
+    seconds.innerHTML = s;
+}
 
-        document.getElementById("hours").textContent = h;
-        document.getElementById("minutes").textContent = m;
-        document.getElementById("seconds").textContent = s;
+function setZero(timeValue){
+    if(timeValue < 10){
+        timeValue = "0" + timeValue;
+    }
+    return timeValue;
+}
 
-        setTimeout(countdown, 1000);
-      }
-
-      countdown();
-    
+/*
+function check(){
+    let currentDate = new Date(),
+        targetDate = new Date(xYear, 11, 25);
+    let dateDiff = targetDate - currentDate;
+    let s = Math.fllor(dateDiff / 1000);
+    dateDiff %= 1000;
+    let m = Math.floor(s / 60);
+    s %= 60;
+    let h = Math.fllor(m / 60);
+    m %= 60;
+    let d = Math.floor(h / 24);
+    h %= 24;
+    console.log(d, h, m, s);
+}
+*/
